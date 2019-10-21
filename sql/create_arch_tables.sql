ce4acbe081e3d4b7bbe5e5b57d3cc824c7b9831a
Pavel Treshnikov <treshnikov@gmail.com>
2019-10-21 10:57:56 +0400

    Update create_arch_tables.sql

--- sql/create_arch_tables.sql
+++ sql/create_arch_tables.sql
@@ -1,9 +1,9 @@
 DO $$
 
 BEGIN
-   FOR counter IN 1..2 LOOP
+   FOR counter IN 1..100 LOOP
 		EXECUTE 'CREATE TABLE public.z_arc' || counter ||' (
-					Timestamp timestamp without time zone NOT NULL,
+					timestamp timestamp without time zone NOT NULL,
 					p_01 double precision,
 					s_01 numeric(22,0),
 					primary key (Timestamp)
